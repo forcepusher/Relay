8df5ca3b5250e6a69adfa59d86d9dcff6c0aa85d
forcepusher <bionitsoup@gmail.com>
2026-06-05 01:59:27 +0700

    Remove comments

--- Packages/com.bananaparty.websocketrelay/Runtime/Serialization/IStateStorage.cs
+++ Packages/com.bananaparty.websocketrelay/Runtime/Serialization/IStateStorage.cs
@@ -2,10 +2,8 @@ using UnityEngine;
 
 namespace BananaParty.WebSocketRelay
 {
-    // Key is either string for JSON or int32 for binary
     public interface IStateStorage<TKey>
     {
-        // Write and Read for every value type
         void WriteInt(TKey key, int value);
         int ReadInt(TKey key);
 
