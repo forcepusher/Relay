66440ebf4946152c1a816177e93586206c7a6c54
forcepusher <bionitsoup@gmail.com>
2026-06-05 01:51:39 +0700

    Add basic types

--- Packages/com.bananaparty.websocketrelay/Runtime/Serialization/IStateStorage.cs
+++ Packages/com.bananaparty.websocketrelay/Runtime/Serialization/IStateStorage.cs
@@ -6,5 +6,23 @@ namespace BananaParty.WebSocketRelay
         // Write and Read for every value type
         void WriteInt(TKey key, int value);
         int ReadInt(TKey key);
+
+        void WriteFloat(TKey key, float value);
+        float ReadFloat(TKey key);
+
+        void WriteBool(TKey key, bool value);
+        bool ReadBool(TKey key);
+
+        void WriteString(TKey key, string value);
+        string ReadString(TKey key);
+
+        void WriteLong(TKey key, long value);
+        long ReadLong(TKey key);
+
+        void WriteDouble(TKey key, double value);
+        double ReadDouble(TKey key);
+
+        void WriteByte(TKey key, byte value);
+        byte ReadByte(TKey key);
     }
 }
