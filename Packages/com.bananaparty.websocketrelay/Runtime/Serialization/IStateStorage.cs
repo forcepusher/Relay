b6f5e8db17a50291f8d977abd397505e256eec90
forcepusher <bionitsoup@gmail.com>
2026-06-05 01:56:24 +0700

    Add common types

--- Packages/com.bananaparty.websocketrelay/Runtime/Serialization/IStateStorage.cs
+++ Packages/com.bananaparty.websocketrelay/Runtime/Serialization/IStateStorage.cs
@@ -1,3 +1,5 @@
+using UnityEngine;
+
 namespace BananaParty.WebSocketRelay
 {
     // Key is either string for JSON or int32 for binary
@@ -16,13 +18,34 @@ namespace BananaParty.WebSocketRelay
         void WriteString(TKey key, string value);
         string ReadString(TKey key);
 
+        void WriteByte(TKey key, byte value);
+        byte ReadByte(TKey key);
+
         void WriteLong(TKey key, long value);
         long ReadLong(TKey key);
 
         void WriteDouble(TKey key, double value);
         double ReadDouble(TKey key);
 
-        void WriteByte(TKey key, byte value);
-        byte ReadByte(TKey key);
+        void WriteVector2(TKey key, Vector2 value);
+        Vector2 ReadVector2(TKey key);
+
+        void WriteVector3(TKey key, Vector3 value);
+        Vector3 ReadVector3(TKey key);
+
+        void WriteVector4(TKey key, Vector4 value);
+        Vector4 ReadVector4(TKey key);
+
+        void WriteVector2Int(TKey key, Vector2Int value);
+        Vector2Int ReadVector2Int(TKey key);
+
+        void WriteVector3Int(TKey key, Vector3Int value);
+        Vector3Int ReadVector3Int(TKey key);
+
+        void WriteQuaternion(TKey key, Quaternion value);
+        Quaternion ReadQuaternion(TKey key);
+
+        void WriteColor(TKey key, Color value);
+        Color ReadColor(TKey key);
     }
 }
