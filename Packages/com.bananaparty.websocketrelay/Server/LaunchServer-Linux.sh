fe69e225e0d1d150d8eea57ea8fda18c69e5a525
forcepusher <bionitsoup@gmail.com>
2026-06-04 17:57:06 +0700

    Fix the warnings

--- Packages/com.bananaparty.websocketrelay/Server/LaunchServer-Linux.sh
+++ Packages/com.bananaparty.websocketrelay/Server/LaunchServer-Linux.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-BUN_PATH="./Bun/bun-linux-x64/bun"
-"$BUN_PATH" ./Source/index.ts
+SCRIPT_DIR="$(cd "$(dirname "$0")" && pwd)"
+BUN_PATH="$SCRIPT_DIR/Bun/bun-linux-x64/bun"
+"$BUN_PATH" --cwd "$SCRIPT_DIR/../.." com.bananaparty.websocketrelay/Server/Source/index.ts
--- Packages/com.bananaparty.websocketrelay/Server/LaunchServer-Windows.bat
+++ Packages/com.bananaparty.websocketrelay/Server/LaunchServer-Windows.bat
@@ -1,4 +1,4 @@
 @echo off
-set BUN_PATH=.\Bun\bun-windows-x64\bun.exe
-"%BUN_PATH%" .\Source\index.ts
+set BUN_PATH=%~dp0Bun\bun-windows-x64\bun.exe
+"%BUN_PATH%" --cwd "%~dp0..\..\." com.bananaparty.websocketrelay\Server\Source\index.ts
 pause
