6248b562e52e0421dbc6d87fd3bcdb3c7766f5c9
forcepusher <bionitsoup@gmail.com>
2026-06-05 23:19:42 +0700

    Work on stuffs

--- Packages/com.bananaparty.websocketrelay/Runtime/Serialization/StateGraphNode.cs
+++ Packages/com.bananaparty.websocketrelay/Runtime/Serialization/StateGraphNode.cs
@@ -1,4 +1,5 @@
 using System.Collections.Generic;
+using UnityEngine;
 
 namespace BananaParty.WebSocketRelay
 {
@@ -35,9 +36,60 @@ namespace BananaParty.WebSocketRelay
                 stateGraphNode.Deserialize(stateStream);
         }
 
-        public void OutputCurrentStateToJson()
-        {
+        // // Pile of shit to optimize for
+        // public string GetJson()
+        // {
+        //     var sb = new System.Text.StringBuilder();
+        //     sb.Append("{");
 
-        }
+        //     sb.Append("\"states\": [");
+        //     for (int i = 0; i < _states.Count; i++)
+        //     {
+        //         var state = _states[i];
+        //         object nameObj = GetValueByReflection(state, "Name");
+        //         object valueObj = GetValueByReflection(state, "Value");
+
+        //         string name = nameObj?.ToString() ?? "Unknown";
+
+        //         sb.Append($"{{\"name\": \"{EscapeJsonString(name)}\", \"value\": {FormatJsonValue(valueObj)}}}");
+        //         if (i < _states.Count - 1) sb.Append(", ");
+        //     }
+        //     sb.Append("], ");
+
+        //     sb.Append("\"children\": [");
+        //     for (int i = 0; i < _children.Count; i++)
+        //     {
+        //         sb.Append(_children[i].GetJson());
+        //         if (i < _children.Count - 1) sb.Append(", ");
+        //     }
+        //     sb.Append("]");
+
+        //     sb.Append("}");
+        //     return sb.ToString();
+        // }
+
+        // private object GetValueByReflection(object obj, string memberName)
+        // {
+        //     var type = obj.GetType();
+        //     var prop = type.GetProperty(memberName);
+        //     if (prop != null) return prop.GetValue(obj);
+        //     var field = type.GetField(memberName);
+        //     if (field != null) return field.GetValue(obj);
+        //     return null;
+        // }
+
+        // private string FormatJsonValue(object value)
+        // {
+        //     if (value == null) return "null";
+        //     if (value is bool b) return b ? "true" : "false";
+        //     if (value is int || value is float || value is double || value is long || value is byte) return value.ToString();
+
+        //     return $"\"{EscapeJsonString(value.ToString() ?? "")}\"";
+        // }
+
+        // private string EscapeJsonString(string s)
+        // {
+        //     return s.Replace("\\", "\\\\").Replace("\"", "\\\"").Replace("\n", "\\n").Replace("\r", "\\r");
+        // }
     }
 }
---
+++ Packages/com.bananaparty.websocketrelay/Runtime/Serialization/Types/ListState.cs
@@ -0,0 +1,30 @@
+using System.Collections.Generic;
+
+namespace BananaParty.WebSocketRelay
+{
+    public class ListState<T> : IState where T : ISerializableState
+    {
+        public List<T> Value;
+        public readonly string Name;
+
+        public ListState(List<T> initialValue, string name = nameof(IntegerState))
+        {
+            Value = initialValue;
+            Name = name;
+        }
+
+        public void Serialize(IStateStream stateStream)
+        {
+            stateStream.WriteInt(Value.Count);
+            foreach (T item in Value)
+            {
+                item.Serialize(stateStream);
+            }
+        }
+
+        public void Deserialize(IStateStream stateStream)
+        {
+            Value = stateStream.ReadInt();
+        }
+    }
+}
