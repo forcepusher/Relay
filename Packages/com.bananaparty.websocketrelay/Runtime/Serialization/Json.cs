4a1cafef12f7b7fa4bd9c81b332bc1ddd044d228
forcepusher <bionitsoup@gmail.com>
2026-06-06 05:35:24 +0700

    Concepting

--- Packages/com.bananaparty.websocketrelay/Runtime/Serialization/Json.cs
+++ Packages/com.bananaparty.websocketrelay/Runtime/Serialization/Json.cs
@@ -2,9 +2,26 @@ namespace BananaParty.WebSocketRelay
 {
     public static class Json
     {
-        public static string ConvertToText(string name, object value, bool quotes = false)
+        public static string ConvertToText(string name, object value)
         {
+            if (value == null)
+                return $"\"{name}\": null";
 
+            string formattedValue;
+            if (value is string str)
+            {
+                formattedValue = $"\"{str.Replace("\\", "\\\\").Replace("\"", "\\\"")}\"";
+            }
+            else if (value is bool b)
+            {
+                formattedValue = b.ToString().ToLower();
+            }
+            else
+            {
+                formattedValue = value.ToString();
+            }
+
+            return $"\"{name}\": {formattedValue}";
         }
     }
 }
