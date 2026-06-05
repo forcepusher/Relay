ba80ca79e11f8759bc754c41bb7f02f0ac918eec
forcepusher <bionitsoup@gmail.com>
2026-06-05 20:25:15 +0700

    Concepting

--- Packages/com.bananaparty.websocketrelay/Runtime/Serialization/StateNode.cs
+++ Packages/com.bananaparty.websocketrelay/Runtime/Serialization/StateNode.cs
@@ -17,36 +17,4 @@ namespace BananaParty.WebSocketRelay
             _states.Add(state);
         }
     }
-
-    // public class StateGraph
-    // {
-    //     bool isWritingArray = false;
-
-    //     private readonly Dictionary<string, object> _identitySateQueue = new();
-
-    //     public void WriteObject(string key, object stateObject)
-    //     {
-    //         _identitySateQueue[key] = stateObject;
-    //     }
-
-    //     public void StartArray(string key)
-    //     {
-    //         isWritingArray = true;
-    //     }
-
-    //     public void StopArray()
-    //     {
-    //         isWritingArray = false;
-    //     }
-
-    //     public object ReadState(string key)
-    //     {
-    //         return _identitySateQueue[key];
-    //     }
-
-    //     public bool HasState(string key)
-    //     {
-    //         return _identitySateQueue.ContainsKey(key);
-    //     }
-    // }
 }
