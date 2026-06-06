8d7d3d8aef527dc27e46bbe190830a790e5027d8
forcepusher <bionitsoup@gmail.com>
2026-06-06 21:29:53 +0700

    Add data graph

---
+++ Packages/com.bananaparty.websocketrelay/Runtime/Nodes/IDataGraph.cs
@@ -0,0 +1,9 @@
+namespace BananaParty.WebSocketRelay
+{
+    public interface IDataGraph<T>
+    {
+        void StartChildGroup(string name);
+        void Write(string name, T data)
+        void EndChildGroup();
+    }
+}
---
+++ Packages/com.bananaparty.websocketrelay/Runtime/Nodes/JsonDaraGraph.cs
@@ -0,0 +1,22 @@
+namespace BananaParty.WebSocketRelay
+{
+    public class JsonDataGraph : IDataGraph<string>
+    {
+
+
+        public void StartChildGroup(string name)
+        {
+
+        }
+
+        public void Write(string name, string data)
+        {
+
+        }
+
+        public void EndChildGroup()
+        {
+
+        }
+    }
+}
