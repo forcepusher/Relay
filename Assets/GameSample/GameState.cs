a6a1c10d2241254eff270f3ec753f49e87012d79
forcepusher <bionitsoup@gmail.com>
2026-06-06 17:41:02 +0700

    Concepting

--- Assets/GameSample/GameState.cs
+++ Assets/GameSample/GameState.cs
@@ -5,18 +5,24 @@ namespace BananaParty.WebSocketRelay.Samples
 {
     public class GameState : MonoBehaviour
     {
-        private IntegerObjectNode _playTimeState = new(nameof(_playTimeState), 0);
+        [SerializeField]
+        private List<Character> _initialCharacters = new();
+
+        private IntegerObjectNode _playTime = new(nameof(_playTime), 0);
         //private int _playTime = 0;
 
-        List<Character> _characters = new();
+        ArrayObjectNode<Character> _characters;
         //List<ItemSpawn> _itemPickups = new();
 
         private IObjectNode _objectNode;
         private readonly IStateStream _stateStream = new JsonStateStream();
         public void Awake()
         {
+            _characters = new ArrayObjectNode<Character>(nameof(_characters), _initialCharacters);
+
             _objectNode = new ObjectNode("GameState",
-                _playTimeState
+                _playTime,
+                _characters
             );
 
             Debug.Log(_objectNode.OutputNameAndValue());
