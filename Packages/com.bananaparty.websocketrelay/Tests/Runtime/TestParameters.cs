68c136980facfccf4afa3704f7c75ef690f6d247
forcepusher <bionitsoup@gmail.com>
2026-06-07 19:44:00 +0700

    Add tests

--- Packages/com.bananaparty.websocketrelay/Tests/Runtime/TestParameters.cs
+++ Packages/com.bananaparty.websocketrelay/Tests/Runtime/TestParameters.cs
@@ -4,6 +4,8 @@ namespace BananaParty.WebSocketRelay.Tests
 {
     public static class TestParameters
     {
+        public const int RelayServerPort = 23144; // Leet for RELAY
+
         public const float ConnectTimeoutThreshold = 3f;
         public const float ReceiveTimeoutThreshold = 5f;
         public const float DisconnectTimeoutThreshold = 3f;
