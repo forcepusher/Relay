495644dae5066d649498731bdc653c9d6cf98e0a
forcepusher <bionitsoup@gmail.com>
2026-06-12 00:45:48 +0700

    Add network placeholder

---
+++ Assets/GameSample/Network.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+namespace BananaParty.WebSocketRelay.Samples
+{
+    public class Network : MonoBehaviour
+    {
+        public void StartServer()
+        {
+
+        }
+
+        public void StartClient()
+        {
+
+        }
+    }
+}
